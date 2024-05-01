09374671497fbf1886233d6322cfeb4f916262a2
0x4E43 <nimaic.dev@gmail.com>
2024-05-01 15:33:25 +0530

    API Integration

--- src/components/logged-in.tsx
+++ src/components/logged-in.tsx
@@ -1,7 +1,48 @@
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
-
+import { useState } from "react";
+import { sendBulkEmail } from "../data/data-service";
 export default function LoggedIn() {
+  const selectionList: string[] = ["Nimai", "Charan", "Maikap"];
   const { user, logout } = useKindeAuth();
+
+  const [template, setTemplate] = useState("");
+  const [myFile, setMyFile] = useState<File | null>(null);
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(event.target.value);
+    setTemplate(event.target.value);
+  };
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    console.log(file);
+    if (file) {
+      setMyFile(file);
+    }
+  };
+
+  const handleSubmit = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    if (!myFile) {
+      alert("Please select a file.");
+      return;
+    }
+    if (!template) {
+      if (selectionList.length > 0) {
+        setTemplate(selectionList[0]);
+        console.log("template set");
+      } else {
+        alert("Please create template in elastic email account");
+      }
+    }
+
+    // Perform your file upload logic here
+    console.log("Template:", selectionList[0]);
+    console.log("File:", myFile);
+
+    //call APIs
+    sendBulkEmail(myFile, template);
+  };
 
   return (
     <>
@@ -46,28 +87,50 @@ export default function LoggedIn() {
                   <strong>Elastic Email</strong>
                 </span>
               </div>
-              <select className="select select-bordered">
-                <option disabled selected>
+              <select
+                className="select select-bordered"
+                onChange={handleChange}
+                value={template}
+              >
+                <option selected disabled>
                   Pick one
                 </option>
-                <option>Star Wars</option>
-                <option>Harry Potter</option>
-                <option>Lord of the Rings</option>
-                <option>Planet of the Apes</option>
-                <option>Star Trek</option>
+                {selectionList.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
               </select>
             </label>
             <label className="form-control w-full max-w-md">
               <div className="label">
-                <span className="label-text">Pick a file with email list</span>
+                <span className="label-text">
+                  Pick a file with email list (
+                  <a
+                    href="/public/email.txt"
+                    className="text-blue-500"
+                    download={true}
+                  >
+                    sample-file
+                  </a>
+                  )
+                </span>
               </div>
               <input
                 type="file"
+                accept=".txt"
                 className="file-input file-input-bordered w-full max-w-md"
+                onChange={handleFileUpload}
               />
             </label>
             <div className="form-control w-full max-w-md my-2">
-                <button className="btn btn-outline">Send Emails</button>
+              <button
+                type="submit"
+                className="btn btn-outline"
+                onClick={handleSubmit}
+              >
+                Send Emails
+              </button>
             </div>
           </form>
         </div>
--- src/data/data-service.ts
+++ src/data/data-service.ts
@@ -33,3 +33,73 @@ export async function fetchEmailTemplateList(): Promise<EmailTemplateModel[]> {
         throw error;
     }
 }
+
+export async function sendBulkEmail(myFile: File, myTemplate : string){
+    const recJSON = await processFile(myFile);
+    const contacts: string[] = []
+    recJSON.forEach((contact) => {
+        contacts.push(JSON.stringify(contact));
+    }); 
+    console.log("Contacts", contacts);
+    const contentBody = { 
+        "From": "admin1@mobile360review.com",
+        "TemplateName": "Hello World",
+    };
+    
+    if(recJSON.length > 0){
+        const reqBody = {
+            "Recipients" : recJSON,
+            "Content": JSON.stringify(contentBody)
+        };
+
+        console.log("Reqest Body: "+ reqBody.Recipients);
+        console.log("Reqest Content: "+ reqBody.Content);
+
+        try {
+            const url = config.BASE_URL + '/emails';
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'X-ElasticEmail-ApiKey': config.API_KEY,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(reqBody)
+            });
+    
+            console.log("Response", response)
+            if (!response.ok) {
+                throw new Error(`Failed to fetch email template list: ${response.statusText}`);
+            }
+            const data = await response.json();
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.error("Error occurred:", error);
+            // You might want to throw an error here or handle it accordingly
+            throw error;
+        }
+    }
+}
+
+export async function processFile(myFile: File): Promise<{ Email: string }[]>{
+    return new Promise<{ Email: string }[]>((resolve, reject) => {
+        const emailList: string[] = [];
+        const fileReader = new FileReader();
+        fileReader.onload = (event: ProgressEvent<FileReader>) => {
+            if (event.target?.result) {
+                const fileContent = event.target.result.toString();
+                console.log("File Content: " + fileContent.split(","));
+                fileContent.split(",").forEach((email) => {
+                    emailList.push(email);
+                });
+                const emailObjects = emailList.map((email) => ({ Email: email.replace('\n', '') }));
+                console.log(emailObjects);
+                resolve(emailObjects);
+            } else {
+                console.log("Failed to read file");
+                reject(new Error("Failed to read file"));
+            }
+        };
+        fileReader.readAsText(myFile);
+    });
+}
